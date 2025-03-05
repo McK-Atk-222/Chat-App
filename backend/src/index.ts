5876094c2da1e8c7931fc2516062549d316e8ea8
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-03-04 18:44:42 -0700

    auth routes and controllers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,22 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoute.js";
+import messageRoutes from "./routes/messageRoute.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
