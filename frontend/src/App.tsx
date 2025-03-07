90fa02a70237bd6b3ebbdfdfbd5fd16fa3c93ecf
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-03-06 19:17:01 -0700

    authContext added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useAuthContext } from './context/authContext'
+import Home from './pages/home/Home';
 import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp';
 
 function App() {
+  const {authUser, isLoading } = useAuthContext();
+  
+  if(isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Login />
+      <Routes>
+          <Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+          <Route path='/signup' element={!authUser ? <SignUp /> : <Navigate to={"/"} />} />
+          <Route path='/login' element={!authUser ? <Login /> : <Navigate to={"/"} />} />
+      </Routes>
     </div>
   )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://Localhost:5000',
+      }
+    }
+  }
 })
