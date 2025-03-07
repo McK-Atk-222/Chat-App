20852641c37f1636f8d005d5a700908d61ba9c12
beanbean <gracefernandez00@icloud.com>
2025-03-06 19:12:28 -0700

    message routes

--- backend/src/routes/messageRoute.ts
+++ backend/src/routes/messageRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
+import protectRoute from "../middleware/protectRoute.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 const router = express.Router();
 
-router.get("/conversations", (req, res) => {
-    res.send("Conversation route");
-})
+router.get("/get/conversations", protectRoute, getUsersForSidebar);
+router.get("/get/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
