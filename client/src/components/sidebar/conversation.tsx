f49c9af9d49a6a7a795e66228dc3cb83f29f4281
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-03-11 21:13:37 -0600

    debugging and style changing

--- client/src/components/sidebar/conversation.tsx
+++ client/src/components/sidebar/conversation.tsx
@@ -11,8 +11,8 @@ const Conversation = ({ conversation }: { conversation: ConversationType}) => {
     return (
         <>
         <div
-				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2
-				 py-1 cursor-pointer ${isSelected ? "bg-sky-500" : ""}`}
+				className={`flex gap-2 items-center hover:bg-violet-500 rounded p-2
+				 py-1 cursor-pointer ${isSelected ? "bg-violet-500" : ""}`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
 				<div className={`avatar ${isOnline ? "online" : ""}`}>
